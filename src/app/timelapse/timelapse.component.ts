2e79806b48057c4489940ee689a2d84ea17f4427
Colum Cross <cxc9401@rit.edu>
2020-05-07 20:21:25 -0400

    First push to prod.

--- src/app/timelapse/timelapse.component.ts
+++ src/app/timelapse/timelapse.component.ts
@@ -16,31 +16,11 @@ export interface TimelapseData {
 })
 export class TimelapseComponent {
 
+  images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
   posts;
 
   constructor(@Inject(MAT_DIALOG_DATA) public data: TimelapseData) { 
     this.posts = data.posts;
   }
-
-  images = [62, 83, 466, 965, 982, 1043, 738].map((n) => `https://picsum.photos/id/${n}/900/500`);
-
-  paused = false;
-  unpauseOnArrow = false;
-  pauseOnIndicator = false;
-  pauseOnHover = true;
-
-  @ViewChild('carousel', {static : true}) carousel: NgbCarousel;
-
-  togglePaused() {
-    if (this.paused) {
-      this.carousel.cycle();
-    } else {
-      this.carousel.pause();
-    }
-    this.paused = !this.paused;
-  }
-
-
-
 
 }
